b4a36688aa11285f88109cd254c9a761d7c921c3
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-11-26 14:57:18 -0800

    user starting coding

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import UsersController from "./users/users-controller.js";
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
+UsersController(app);
 
 app.listen(4000);
