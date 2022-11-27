c044d21b768ec82dc68fbfe03bb641599182b39d
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-11-27 01:22:56 -0800

    users component

--- users/users-dao.js
+++ users/users-dao.js
@@ -2,6 +2,12 @@ import usersModel from "./users-model.js";
 
 export const createUser = (user) => usersModel.create(user);
 
+export const register = async (user) => {
+  const existingUser = await findByUsername(user.username);
+  if (existingUser) {
+  }
+};
+
 export const findAllUsers = () => usersModel.find();
 
 export const findUserById = (uid) => usersModel.findById(uid);
@@ -9,7 +15,7 @@ export const findUserById = (uid) => usersModel.findById(uid);
 export const findByUsername = (username) => usersModel.findOne({ username });
 
 export const findByCredentials = (username, password) =>
-  usersModel.findOne({ username, password });
+  usersModel.findOne({ username, password }, { password: false });
 
 export const deleteUser = (uid) => usersModel.deleteOne({ _id: uid });
 
