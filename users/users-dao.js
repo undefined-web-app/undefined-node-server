3d280a658cbdc04f6609dce7dc649e9f588b3339
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-12-02 12:22:18 -0800

    go to other users' profile and session work

--- users/users-dao.js
+++ users/users-dao.js
@@ -21,3 +21,6 @@ export const deleteUser = (uid) => usersModel.deleteOne({ _id: uid });
 
 export const updateUser = (uid, userUpdates) =>
   usersModel.updateOne({ _id: uid }, { $set: userUpdates });
+
+export const findUserByUsername = (username) =>
+  usersModel.findOne({ username });
