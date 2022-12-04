08ee23d6ecd293052197a21da33175a14cca1160
Rayzzzx <56136510+Rayzzzx@users.noreply.github.com>
2022-12-04 00:34:42 -0800

    test deploy

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
-    // origin: "https://main--meek-klepon-26408d.netlify.app",
+    // origin: "http://localhost:3000",
+    origin: "https://main--meek-klepon-26408d.netlify.app",
   })
 );
 app.use(
